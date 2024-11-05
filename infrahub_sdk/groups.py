80cea97286d8af7c93fc381bab71a135b024a6f9
Lucas Guillermou <lucas@opsmill.com>
2024-11-05 12:33:10 +0100

    Add groups.group_add_subscriber

---
+++ infrahub_sdk/groups.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from infrahub_sdk import InfrahubClient
+from infrahub_sdk.node import InfrahubNode
+
+
+async def group_add_subscriber(
+    client: InfrahubClient, group: InfrahubNode, subscribers: List[str], branch: str
+) -> dict:
+    subscribers_str = ["{ id: " + f'"{subscriber}"' + " }" for subscriber in subscribers]
+    query = """
+    mutation {
+        RelationshipAdd(
+            data: {
+                id: "%s",
+                name: "subscribers",
+                nodes: [ %s ]
+            }
+        ) {
+            ok
+        }
+    }
+    """ % (
+        group.id,
+        ", ".join(subscribers_str),
+    )
+
+    return await client.execute_graphql(query=query, branch_name=branch, tracker="mutation-relationshipadd")
