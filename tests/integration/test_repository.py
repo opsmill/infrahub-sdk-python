373793f59a8a8517fca354275251b5332b7f6e85
jeremy brisson <jeremy@opsmill.com>
2025-01-13 12:24:12 +0100

    Add variable to fix repository issue from ci

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -1,31 +1,31 @@
-# from __future__ import annotations
+from __future__ import annotations
 
-# from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING
 
-# import pytest
+import pytest
 
-# from infrahub_sdk.testing.docker import TestInfrahubDockerClient
-# from infrahub_sdk.testing.repository import GitRepo
-# from infrahub_sdk.utils import get_fixtures_dir
+from infrahub_sdk.testing.docker import TestInfrahubDockerClient
+from infrahub_sdk.testing.repository import GitRepo
+from infrahub_sdk.utils import get_fixtures_dir
 
-# if TYPE_CHECKING:
-#     from infrahub_sdk import InfrahubClient
+if TYPE_CHECKING:
+    from infrahub_sdk import InfrahubClient
 
 
-# class TestInfrahubRepository(TestInfrahubDockerClient):
-#     @pytest.fixture(scope="class")
-#     def infrahub_version(self) -> str:
-#         return "1.1.0"
+class TestInfrahubRepository(TestInfrahubDockerClient):
+    @pytest.fixture(scope="class")
+    def infrahub_version(self) -> str:
+        return "1.1.0"
 
-#     async def test_add_repository(self, client: InfrahubClient, remote_repos_dir):
-#         src_directory = get_fixtures_dir() / "integration/mock_repo"
-#         repo = GitRepo(name="mock_repo", src_directory=src_directory, dst_directory=remote_repos_dir)
-#         commit = repo._repo.git[repo._repo.git.head()]
-#         assert len(list(repo._repo.git.get_walker())) == 1
-#         assert commit.message.decode("utf-8") == "First commit"
+    async def test_add_repository(self, client: InfrahubClient, remote_repos_dir):
+        src_directory = get_fixtures_dir() / "integration/mock_repo"
+        repo = GitRepo(name="mock_repo", src_directory=src_directory, dst_directory=remote_repos_dir)
+        commit = repo._repo.git[repo._repo.git.head()]
+        assert len(list(repo._repo.git.get_walker())) == 1
+        assert commit.message.decode("utf-8") == "First commit"
 
-#         response = await repo.add_to_infrahub(client=client)
-#         assert response.get(f"{repo.type.value}Create", {}).get("ok")
+        response = await repo.add_to_infrahub(client=client)
+        assert response.get(f"{repo.type.value}Create", {}).get("ok")
 
-#         repos = await client.all(kind=repo.type)
-#         assert repos
+        repos = await client.all(kind=repo.type)
+        assert repos
