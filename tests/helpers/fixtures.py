a6f936d628b4df17800ed97d0b892c05e2cc2e3c
wvandeun <wim@opsmill.com>
2025-01-20 10:41:51 +0100

    adds `infrahubctl repository list` command

--- tests/helpers/fixtures.py
+++ tests/helpers/fixtures.py
@@ -5,3 +5,12 @@ def get_fixtures_dir() -> Path:
     """Get the directory which stores fixtures that are common to multiple unit/integration tests."""
     here = Path(__file__).parent.resolve()
     return here.parent / "fixtures"
+
+
+def read_fixture(file_name: str, fixture_subdir: str = ".") -> str:
+    """Read the contents of a fixture."""
+    file_path = get_fixtures_dir() / fixture_subdir / file_name
+    with file_path.open("r", encoding="utf-8") as fhd:
+        fixture_contents = fhd.read()
+
+    return fixture_contents
