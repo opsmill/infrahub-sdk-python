e90acb42cad9078bb81322c945eacf0840db7950
Lucas Guillermou <lucas@opsmill.com>
2025-01-22 15:10:50 +0100

    WIP: Run CI tests against multiple versions

--- infrahub_sdk/testing/docker.py
+++ infrahub_sdk/testing/docker.py
@@ -1,10 +1,40 @@
+from __future__ import annotations
+
+import os
+
 import pytest
 from infrahub_testcontainers.helpers import TestInfrahubDocker
+from packaging.version import InvalidVersion, Version
 
 from .. import Config, InfrahubClient, InfrahubClientSync
 
+INFRAHUB_VERSION = os.getenv("INFRAHUB_TESTING_IMAGE_VER", "latest")
+
+
+def skip_version(min_infrahub_version: str | None = None, max_infrahub_version: str | None = None) -> bool:
+    """
+    Check if a test should be skipped depending on infrahub version.
+    """
+    try:
+        version = Version(INFRAHUB_VERSION)
+    except InvalidVersion:
+        # We would typically end up here for development purpose while running a CI test against
+        # unreleased versions of infrahub, like `stable` or `develop` branch.
+        # For now, we consider this means we are testing against the most recent version of infrahub,
+        # so we skip if the test should not be ran against a maximum version.
+        return max_infrahub_version is None
+
+    if min_infrahub_version is not None and version < Version(min_infrahub_version):
+        return True
+
+    return max_infrahub_version is not None and version > Version(max_infrahub_version)
+
 
 class TestInfrahubDockerClient(TestInfrahubDocker):
+    @pytest.fixture(scope="class")
+    def infrahub_version(self) -> str:
+        return INFRAHUB_VERSION
+
     @pytest.fixture(scope="class")
     def client(self, infrahub_port: int) -> InfrahubClient:
         return InfrahubClient(
--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import pytest
-
 from infrahub_sdk.testing.docker import TestInfrahubDockerClient
 from infrahub_sdk.testing.repository import GitRepo
 from infrahub_sdk.utils import get_fixtures_dir
@@ -13,10 +11,6 @@ if TYPE_CHECKING:
 
 
 class TestInfrahubRepository(TestInfrahubDockerClient):
-    @pytest.fixture(scope="class")
-    def infrahub_version(self) -> str:
-        return "1.1.0"
-
     async def test_add_repository(self, client: InfrahubClient, remote_repos_dir):
         src_directory = get_fixtures_dir() / "integration/mock_repo"
         repo = GitRepo(name="mock_repo", src_directory=src_directory, dst_directory=remote_repos_dir)
