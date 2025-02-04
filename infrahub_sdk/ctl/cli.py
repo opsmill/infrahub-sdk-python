800ac86fa48a91f52d8c71411229ba3480962130
Alex Gittings <agitting96@gmail.com>
2025-02-04 09:21:54 +0000

    Initial uv migration

--- infrahub_sdk/ctl/cli.py
+++ infrahub_sdk/ctl/cli.py
@@ -5,7 +5,7 @@ try:
 except ImportError as exc:
     sys.exit(
         f"Module {exc.name} is not available, install the 'ctl' extra of the infrahub-sdk package, `pip install 'infrahub-sdk[ctl]'` or enable the "
-        "Poetry shell and run `poetry install --extras ctl`."
+        "virtual environment and run `uv sync --extras ctl`."
     )
 
 __all__ = ["app"]
