6b6028641eb007ee446a06ca3d981f58c007387c
Aaron McCarty <aaron@opsmill.com>
2025-05-09 08:30:14 -0700

    fix bug in Timestamp.add

--- infrahub_sdk/timestamp.py
+++ infrahub_sdk/timestamp.py
@@ -153,7 +153,7 @@ class Timestamp:
         nanoseconds: int = 0,
         disambiguate: Literal["compatible"] = "compatible",
     ) -> Timestamp:
-        return Timestamp(
+        return self.__class__(
             self._obj.add(
                 years=years,
                 months=months,
