aa8c792ea3f6bca00d2a8f2c8ec269f6eb138b8e
Damien Garros <dgarros@gmail.com>
2025-05-12 15:13:27 +0200

    Expose everything from node module

--- infrahub_sdk/node/__init__.py
+++ infrahub_sdk/node/__init__.py
@@ -1,14 +1,32 @@
 from __future__ import annotations
 
-from .constants import HFID_STR_SEPARATOR
+from .constants import (
+    ARTIFACT_DEFINITION_GENERATE_FEATURE_NOT_SUPPORTED_MESSAGE,
+    ARTIFACT_FETCH_FEATURE_NOT_SUPPORTED_MESSAGE,
+    ARTIFACT_GENERATE_FEATURE_NOT_SUPPORTED_MESSAGE,
+    HFID_STR_SEPARATOR,
+    IP_TYPES,
+    PROPERTIES_FLAG,
+    PROPERTIES_OBJECT,
+    SAFE_VALUE,
+)
 from .node import InfrahubNode, InfrahubNodeBase, InfrahubNodeSync
+from .property import NodeProperty
 from .related_node import RelatedNode, RelatedNodeBase, RelatedNodeSync
 from .relationship import RelationshipManager, RelationshipManagerBase, RelationshipManagerSync
 
 __all__ = [
+    "ARTIFACT_DEFINITION_GENERATE_FEATURE_NOT_SUPPORTED_MESSAGE",
+    "ARTIFACT_FETCH_FEATURE_NOT_SUPPORTED_MESSAGE",
+    "ARTIFACT_GENERATE_FEATURE_NOT_SUPPORTED_MESSAGE",
+    "IP_TYPES",
+    "PROPERTIES_FLAG",
+    "PROPERTIES_OBJECT",
+    "SAFE_VALUE",
     "InfrahubNode",
     "InfrahubNodeBase",
     "InfrahubNodeSync",
+    "NodeProperty",
     "RelatedNode",
     "RelatedNodeBase",
     "RelatedNodeSync",
