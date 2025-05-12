d762fb6c5fb12c9e227edf0d5c8af59c93aad11f
Aaron McCarty <aaron@opsmill.com>
2025-05-12 10:02:59 -0700

    fix .subtract too

--- infrahub_sdk/timestamp.py
+++ infrahub_sdk/timestamp.py
@@ -183,7 +183,7 @@ class Timestamp:
         nanoseconds: int = 0,
         disambiguate: Literal["compatible"] = "compatible",
     ) -> Timestamp:
-        return Timestamp(
+        return self.__class__(
             self._obj.subtract(
                 years=years,
                 months=months,
