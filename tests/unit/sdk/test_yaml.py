19f4deb08eb78a534e6550b857ce38ae2a174f83
Graham Noel <graham.noel@gresearch.co.uk>
2025-06-04 11:38:39 +0100

    Update test_yaml.py
    Update incorrect encoding test after read_file method changes.

--- tests/unit/sdk/test_yaml.py
+++ tests/unit/sdk/test_yaml.py
@@ -16,11 +16,12 @@ def test_read_missing_file() -> None:
 
 
 def test_read_incorrect_encoding() -> None:
-    file = here / "test_data/schema_encoding_error.yml"
-    yaml_file = YamlFile(location=file)
+    file_name = "schema_encoding_error.yml"
+    full_path = here / "test_data" / file_name
+    yaml_file = YamlFile(location=full_path)
     yaml_file.load_content()
     assert not yaml_file.valid
-    assert yaml_file.error_message == f"Unable to read {file} with utf-8 encoding"
+    assert yaml_file.error_message == f"Unable to read {file_name} with utf-8 encoding"
 
 
 def test_read_multiple_files() -> None:
