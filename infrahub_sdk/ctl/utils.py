003d6abe784725dc0409b14d7cc5213478c00712
Alex Gittings <agitting96@gmail.com>
2025-09-09 10:28:19 +0100

    Remove unused import of `get_branch` from `utils.py`

--- infrahub_sdk/ctl/utils.py
+++ infrahub_sdk/ctl/utils.py
@@ -118,7 +118,7 @@ def execute_graphql_query(
     query_str = query_object.load_query()
 
     client = initialize_client_sync()
-    
+
     if not branch:
         branch = client.config.default_infrahub_branch
 
