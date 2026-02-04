6b6cf68c0a0703f96c15891544a655dc87c74e58
Aaron McCarty <ajtmccarty@pm.me>
2026-02-04 10:28:26 -0600

    add str method to JinjaTemplateError (#795)
    * add str method to JinjaTemplateError
    
    * force to str

--- infrahub_sdk/template/exceptions.py
+++ infrahub_sdk/template/exceptions.py
@@ -12,6 +12,9 @@ class JinjaTemplateError(Error):
     def __init__(self, message: str) -> None:
         self.message = message
 
+    def __str__(self) -> str:
+        return str(self.message)
+
 
 class JinjaTemplateNotFoundError(JinjaTemplateError):
     def __init__(self, message: str | None, filename: str, base_template: str | None = None) -> None:
