c4868938af70cb2cd67d45e894d1350083432397
Pol Michel <polmichel29@gmail.com>
2026-02-16 13:28:32 +0100

    docs: fix comments on Icon management IHS-198

--- docs/src/theme/MDXComponents.js
+++ docs/src/theme/MDXComponents.js
@@ -6,5 +6,5 @@ import { Icon } from '@iconify/react'; // Import the entire Iconify library.
 export default {
     // Re-use the default mapping
     ...MDXComponents,
-    Icon: Icon, // Make the iconify Icon component available in MDX as <icon />.
+    Icon: Icon, // Make the iconify Icon component available in MDX as <Icon />.
 };
